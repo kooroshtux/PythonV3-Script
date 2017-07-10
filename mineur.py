62a46249af44ba2de0f78fbcaefab8548a63d1c8
kooroshtux <kooroshtux@gmail.com>
2017-07-10 19:39:49 +0200

    Script qui affiche si vous êtes mineur  ou majeur

---
+++ mineur.py
@@ -0,0 +1,10 @@
+print("Entrer votre âge: ")
+age = input()
+age = 18
+
+if age < 18:
+    print("/!\ Vous êtes mineur")
+else:
+        print(" =) Vous êtes majeur")
+
+input("Appuyer sur  <ENTER> pour quitter ... ")
